42db59f29f3327e40e91f810bd8020b22b63d0ad
GROX13 <giorgirokhadze@gmail.com>
2014-11-24 23:48:53 +0400

    assign2

--- assign2/Application.js
+++ assign2/Application.js
@@ -18,17 +18,17 @@ router.add("/users/{id:n}/permissions/{permName:s}",
     {
         GET: function(request, response, params){
             response.writeHead(200, {"Content-Type":"text/html", "filename":"index.html"});
-            response.write(data);
+            response.write(params.id + '<br/>' + params.permName);
             response.end();
         },
         POST: function(request, response, params){
             response.writeHead(200, {"Content-Type":"text/html", "filename":"index.html"});
-            response.write(data);
+            response.write(params.id + '<br/>' + params.permName);
             response.end();
         },
         DELETE: function(request, response, params){
             response.writeHead(200, {"Content-Type":"text/html", "filename":"index.html"});
-            response.write(data);
+            response.write(params.id + '<br/>' + params.permName);
             response.end();
         }
     },
@@ -45,8 +45,7 @@ router.add("/{fileName:s}/{fileExt:s}",
             var tmp = type[file.split(".")[1]];
             fileReader.readFile(file, function (error, data) {
                 if (!error) {
-                    response.writeHead(200, {"Content-Type":tmp,
-                        "Content-Disposition":"attachment; filename:" + file});
+                    response.writeHead(200, {"Content-Type":tmp});
                     response.write(data);
                     response.end();
                 }
@@ -58,10 +57,10 @@ router.add("/{fileName:s}/{fileExt:s}",
         response.end();
     });
 
-router.add("/home/{fileName:s}/{fileExt:s}",
+router.add("/download/{fileName:s}/{fileExt:s}",
     {
         GET: function(request, response, params) {
-            var file = request.url.substr(1, request.url.length);
+            var file = request.url.substr(10, request.url.length);
             file = file.replace("/", ".");
             fileReader.readFile(file, function (error, data) {
                 if (!error) {
