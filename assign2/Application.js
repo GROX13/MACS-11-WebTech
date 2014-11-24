7d74a63ef633872e81816fd2f8199517361c2cc8
GROX13 <giorgirokhadze@gmail.com>
2014-11-24 22:15:55 +0400

    assign2

--- assign2/Application.js
+++ assign2/Application.js
@@ -7,16 +7,11 @@ var fileReader = require('fs');
 var router = new Router();
 var server = new Server();
 
-
-//fileReader.readFile("index.html", function(e, data){
-//    console.log(data.toString());
-//});
-
 router.add("users/{id:n}/permissions/{permName:s}",
     {
         GET: function(request, response, params){
-            response.writeHead(200, {'content-type': 'text/html'});
-            response.write(params.id + '<br/>' + params.permName);
+            response.writeHead(200, {"Content-Type": "text/html", "filename":"index.html"});
+            response.write(data);
             response.end();
         }
     },
@@ -28,14 +23,18 @@ router.add("users/{id:n}/permissions/{permName:s}",
 router.add("/{fileName:s}/{fileExt:s}",
     {
         GET: function(request, response, params) {
-            var file = request.url.substr(1, request.url.length - 1);
+            var file = request.url.substr(1, request.url.length);
             file = file.replace("/", ".");
-            fileReader.readFile(file, function (err, data) {
-                if (err) {
-
-                } else {
-
-                    response.writeHead(200, {"Content-Type": "text/html",
+            fileReader.readFile(file, function (error, data) {
+                if (!error) {
+                    var type = {
+                        "html":"text/html",
+                        "css":"text/css",
+                        "js":"text/js",
+                        "jpg":"image/jpg",
+                        "png":"image/png"
+                    };
+                    response.writeHead(200, {"Content-Type":type[file.split(".")[1]],
                         "Content-Disposition":"attachment; filename:" + file});
                     response.write(data);
                     response.end();
@@ -43,7 +42,7 @@ router.add("/{fileName:s}/{fileExt:s}",
             });
         }
     },
-    function(error){
+    function(request, response, error){
         response.writeHead(500, {});
         response.end();
     });
