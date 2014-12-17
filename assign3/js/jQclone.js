751b6acdf08345e960bffc81ad83eff6dce590cf
GROX13 <giorgirokhadze@gmail.com>
2014-12-17 16:31:06 +0400

    assign3

--- assign3/js/jQclone.js
+++ assign3/js/jQclone.js
@@ -20,29 +20,38 @@
 // your implementation here
 
     function first() {
-        return setMethods(this[0]);
+        var result = [];
+        if (this.length > 0)
+            result.push(this[0]);
+        return factory(result);
     }
 
     function last() {
-        return setMethods(this[this.length - 1]);
+        var result = [];
+        if (this.length > 0)
+            result.push(this[this.length - 1]);
+        return factory(result);
     }
 
     function each(callback) {
-        for (var i; i < this.length; i++)
-            callback(this[i], i);
+        for (var i = 0; i < this.length; i++)
+            callback.call(this, this[i], i);
         return this;
     }
 
     function find(selector) {
         var result = [];
         for (var i = 0; i < this.length; i++)
-            result.concat(this[i].querySelectorAll(selector));
-        return setMethods(result);
+            result = result.concat(compose(this[i].querySelectorAll(selector)));
+        return factory(result);
     }
 
     function hasClass(className) {
-        var regex = new RegExp("(?:^|\\s)" + className + "(?!\\S)");
-        return this.first().className.match(regex) !== null;
+        var result = false,
+            regex = new RegExp("(?:^|\\s)" + className + "(?!\\S)");
+        if (this.length > 0)
+            result = regex.test(this[0].className);
+        return result;
     }
 
     function addClass(className) {
@@ -89,22 +98,40 @@
         return this[0].getAttribute(name);
     }
 
-    function css(cssStyle) {
-        return undefined;
+    function css(cssStyle, value) {
+        var i;
+        if (typeof value !== 'undefined') {
+            return this[0].getComputedStyle(cssStyle, null);
+        } else {
+            if (typeof cssStyle === Object) {
+                for (i = 0; i < this.length; i++) {
+                    this[i].style = cssStyle;
+                }
+            } else {
+                for (i = 0; i < this.length; i++) {
+                    this[i].style = cssStyle;
+                }
+            }
+        }
+        return this;
     }
 
     function data() {
-
+        if (document.body.dataset) {
+            // modern HTML5 browser
+        } else {
+            // archaic browser, use custom solution
+        }
     }
 
-    function setMethods(element) {
-        element = typeof element !== 'undefined' ? element : [];
+    function factory(element) {
         element.first = first;
         element.last = last;
         element.each = each;
         element.find = find;
         element.hasClass = hasClass;
         element.addClass = addClass;
+
         element.removeClass = removeClass;
         element.toggleClass = toggleClass;
         element.attr = attr;
@@ -113,11 +140,22 @@
         return element;
     }
 
+    function compose(element) {
+        var result = [];
+        for (var i = 0; i < element.length; i++)
+            result.push(element[i]);
+        return result;
+    }
+
 // expose jQClone to global scope
-    window.jQClone = function (selector, search) {
+    window.JQClone = function (selector, search) {
         search = typeof search !== 'undefined' ? search : document;
-        var result = search.querySelectorAll(selector);
-        return setMethods(result);
+        var result = [];
+        if (typeof selector !== 'undefined') {
+            var local = search.querySelectorAll(selector);
+            result = compose(local);
+        }
+        return factory(result);
     };
-    window.$ = window.jQClone;
+    window.$ = window.JQClone;
 })(window, document);
